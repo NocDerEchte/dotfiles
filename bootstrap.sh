be9e7d3ee1436e93533776a107c635f2dae4e719
NocDerEchte <147306991+NocDerEchte@users.noreply.github.com>
2025-08-01 12:37:46 +0200

    refactor: switch from .zprofile to .zshenv
    .zshenv loads whenever zsh is started not only on login shells (like
    .zprofile does)

--- bootstrap.sh
+++ bootstrap.sh
@@ -82,8 +82,8 @@ _link_configs() {
     _print_info "Created symlink: ${dir} -> ${dest_dir}"
   done
 
-  # Create symlink for .zprofile
-  ln -sf "${DOTFILES_DIR}/.zprofile" "${HOME}/.zprofile"
+  # Create symlink for .zshenv
+  ln -sf "${DOTFILES_DIR}/.zshenv" "${HOME}/.zshenv"
 }
 
 function _link_fonts() {
